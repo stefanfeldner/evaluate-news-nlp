20b3f152f6198d8da5e8e712df3100fb3d222252
stefanfeldner@gmail.com <stefanfeldner@gmail.com>
2020-08-09 00:01:59 +0200

    Adding server routes

--- src/server/index.js
+++ src/server/index.js
@@ -1,41 +1,83 @@
-var path = require('path');
-const express = require('express');
-const mockAPIResponse = require('./mockAPI.js');
+var path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const fetch = require("node-fetch");
 
 const app = express();
 
-app.use(express.static('dist'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(express.static("dist"));
 
 console.log(__dirname);
 
-// Require the Aylien npm package
-var AYLIENTextAPI = require('aylien_textapi');
-
-const dotenv = require('dotenv');
-const { response } = require('express');
+const dotenv = require("dotenv");
+const { response } = require("express");
 dotenv.config();
 
-const textapi = new AYLIENTextAPI({
-  application_id: process.env.API_ID,
-  application_key: process.env.API_KEY,
+const apiKey = process.env.API_KEY;
+
+app.get("/", function (req, res) {
+    res.sendFile("dist/index.html");
 });
 
-app.get('/', function (req, res) {
-    res.sendFile('dist/index.html');
+const apiData = {};
+
+app.post("/form", async function (req, res) {
+    apiData.formText = req.body.formText;
+
+    console.log("Link to extract article from: " + apiData.formText);
+
+    await fetchMeaningcloudApi(fetchURL(apiKey, apiData.formText));
+
+    res.status(200).send({
+        success: true,
+        message: "Data saved successfully",
+        data: apiData,
+    });
 });
 
-const data = []
+const fetchURL = (key, txt) => {
+    return `https://api.meaningcloud.com/sentiment-2.1?&key=${key}&of=json&lang=en&verbose=y&txt=${txt}`;
+};
 
-app.post('/form', function (req, res) {
-    console.log(req.body)
-    data.push(req.body)
-})
+const analyzeData = {};
+
+const fetchMeaningcloudApi = async (url) => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        // console.log(data);
+
+        if ("subjectivity" in data) {
+            analyzeData.subjectivity = data.subjectivity;
+            analyzeData.irony = data.irony;
+        } else {
+            throw new Error("Subjectivity empty");
+        }
+        console.log('Analysis Result:');
+        console.log(analyzeData);
+    } catch (err) {
+        console.log("Error: " + err);
+    }
+};
+
+app.get("/getData", (req, res) => {
+    // sending projectData
+    res.status(200).send(analyzeData);
+    console.log("DATA SENT");
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Example app listening on port 8081!');
+    console.log("Example app listening on port 8081!");
 });
 
-app.get('/test', function (req, res) {
+app.get("/test", function (req, res) {
     res.send(mockAPIResponse);
 });
+
+module.exports = app;
