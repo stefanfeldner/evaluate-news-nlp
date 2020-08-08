db972a46d5b74087132cb93d77fbb0352d4d4f08
stefanfeldner@gmail.com <stefanfeldner@gmail.com>
2020-08-09 00:23:27 +0200

    Fixing test bug

--- src/__tests__/formHandler.test.js
+++ src/__tests__/formHandler.test.js
@@ -1,8 +1,10 @@
-const handleSubmit = '../client/js/formHandler';
 import "babel-polyfill";
+import { handleSubmit } from '../client/js/formHandler';
 
 describe('Testing if function exists' , () => {
     test('Should return true', async () => {
         expect(typeof handleSubmit).toBe('function');
+        expect(handleSubmit).toBeTruthy();
+        expect(handleSubmit).toBeDefined();
     });
 });
